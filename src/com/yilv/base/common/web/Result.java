2d7b1193606c467f9bcc1dbe88cb728160424212
meikalong <meikalong>
2016-03-21 16:02:11 +0800

    统一返回格式
    Signed-off-by: meikalong <meikalong>

--- src/com/yilv/base/common/web/Result.java
+++ src/com/yilv/base/common/web/Result.java
@@ -3,8 +3,11 @@ package com.yilv.base.common.web;
 public class Result {
 	private int code;
 	private String message;
-	public static final String FAILED = "failed";
-	public static final String SUCCESS = "success";
+	public static final int FAILED = 500;
+	public static final int SUCCESS = 200;
+
+	public static final String FAILED_MSG = "failed";
+	public static final String SUCCESS_MSG = "success";
 
 	public Result() {
 
